c2d4b9e0939772dc618844c6e3081a205a53eef6
Avishek Garain <35271336+garain@users.noreply.github.com>
2020-06-07 13:19:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,13 +6,22 @@ from krms_iris import *
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/')  
+def upload():  
+    return render_template("file_upload_form.html")  
+ 
+@app.route('/success', methods = ['POST'])  
+def success():  
+    if request.method == 'POST':  
+        f = request.files['file']  
+        f.save(f.filename)  
+        return render_template("success.html", name = f.filename)  
+@app.route('/results')
 def index():
     """Return homepage."""
     json_data = main()#{'Hello': 'World!'}
     #return jsonify(json_data)
-    return jsonify(json_data)
-    
+    return jsonify(json_data)     
 
 if __name__ == '__main__':
     app.run()
